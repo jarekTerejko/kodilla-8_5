471c0903d90ce6ef5b27bce6e28d453b5fa01b88
Jarosław Terejko <j.terejko@gmail.com>
2017-08-15 12:01:02 +0200

    add files and folder

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// triangleArea 
+var a = 5;
+var h = 10;
+var triangleArea;
+triangleArea = a*h/2;
+console.log(triangleArea);
+
+
+// triangleArea with user numbers
+var a = prompt('Enter a number for variable a');
+var h = prompt('Enter a number for variable h');
+var triangleArea;
+triangleArea = a*h/2;
+console.log(triangleArea);
